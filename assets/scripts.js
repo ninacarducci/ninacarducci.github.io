288cb87a0ec0a70eaf901f70b5e8bc6f3e9d3ca7
ninacarducci <105047782+ninacarducci@users.noreply.github.com>
2022-05-06 04:45:38 -0500

    First Commit

---
+++ assets/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.gallery').mauGallery({
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
+        lightBox: true,
+        lightboxId: 'myAwesomeLightbox',
+        showTags: true,
+        tagsPosition: 'top'
+    });
+});
