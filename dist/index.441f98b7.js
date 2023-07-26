7c9258aab7b991bf93c41cdae5fdacc10390cd00
Léo Lecée <leo.lecee@gmail.com>
2023-07-26 16:45:38 +0700

    Build with Parcel

---
+++ dist/index.441f98b7.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".gallery").mauGallery({
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
+        lightBox: true,
+        lightboxId: "myAwesomeLightbox",
+        showTags: true,
+        tagsPosition: "top"
+    });
+});
+
+//# sourceMappingURL=index.441f98b7.js.map
